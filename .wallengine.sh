ce3ce84d1eb80b84b096967d90524ca5b073e90f
V <119004237+V2Valerie@users.noreply.github.com>
2023-09-17 21:15:44 +0330

    Add files via upload

---
+++ .wallengine.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+PIDFILE=/tmp/wallpaper.pid
+
+# Check if PID file exists and if the process is still running
+if [ -f "$PIDFILE" ] && kill -0 $(cat "$PIDFILE") >/dev/null 2>&1; then
+   echo "Script is already running"
+   exit 1
+fi
+
+# Create PID file
+echo $$ > "$PIDFILE"
+
+while true; do
+    feh --randomize --bg-fill ~/Git/gruvbox-wallpapers/
+    sleep 120  # Sleep for 2 minutes (120 seconds)
+done
+
+# Clean up PID file
+rm "$PIDFILE"
